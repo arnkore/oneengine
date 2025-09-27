d96fad19d8313aa5a3d6a9f618e1f488a3be3a72
arnkore <ludo.arnk@gmail.com>
2025-09-27 22:56:11 +0800

    license: 为所有源文件添加Apache 2.0许可证头部
    - 为所有Rust源文件添加标准的Apache 2.0许可证头部
    - 包括src/、examples/、tests/、benches/目录下的所有.rs文件
    - 总共处理了109个源文件
    - 使用标准的Apache Software Foundation许可证声明格式
    - 确保所有代码都符合Apache 2.0许可证要求
    
    许可证头部包含：
    - Apache Software Foundation版权声明
    - Apache 2.0许可证条款
    - 许可证链接和免责声明

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 //! 网络模块
 //! 
 //! 包含网络服务器、零拷贝序列化、压缩等功能
